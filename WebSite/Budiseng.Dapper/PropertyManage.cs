96ff14b37cc8f1c0e9b293be891e5f1e9388a72b
budaiseng <budaiseng@email.com>
2015-04-11 21:39:55 +0800

    dapper

---
+++ WebSite/Budiseng.Dapper/PropertyManage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budiseng.Dapper
+{
+   public class PropertyManage
+   {
+        public static string GetSqlPairs
+    (IEnumerable<string> keys, string separator = ", ")
+        {
+            var pairs = keys.Select(key => string.Format("{0}=@{0}", key)).ToList();
+            return string.Join(separator, pairs);
+        }
+
+        public  static void SetId<T>(T obj, int id, IDictionary<string, object> propertyPairs)
+        {
+            if (propertyPairs.Count == 1)
+            {
+                var propertyName = propertyPairs.Keys.First();
+                var propertyInfo = obj.GetType().GetProperty(propertyName);
+                if (propertyInfo.PropertyType == typeof(int))
+                {
+                    propertyInfo.SetValue(obj, id, null);
+                }
+            }
+        }
+        /// <summary>
+        /// Retrieves a Dictionary with name and value 
+        /// for all object properties matching the given criteria.
+        /// </summary>
+        public static PropertyContainer ParseProperties<T>(T obj)
+        {
+            var propertyContainer = new PropertyContainer();
+
+            var typeName = typeof(T).Name;
+            var validKeyNames = new[] { "Id", 
+            string.Format("{0}Id", typeName), string.Format("{0}_Id", typeName) };
+
+            var properties = typeof(T).GetProperties();
+            foreach (var property in properties)
+            {
+                // Skip reference types (but still include string!)
+                if (property.PropertyType.IsClass && property.PropertyType != typeof(string))
+                    continue;
+
+                // Skip methods without a public setter
+                if (property.GetSetMethod() == null)
+                    continue;
+
+                // Skip methods specifically ignored
+                if (property.IsDefined(typeof(DapperIgnore), false))
+                    continue;
+
+                var name = property.Name;
+                var value = typeof(T).GetProperty(property.Name).GetValue(obj, null);
+
+                if (property.IsDefined(typeof(DapperKey), false) || validKeyNames.Contains(name))
+                {
+                    propertyContainer.AddId(name, value);
+                }
+                else
+                {
+                    propertyContainer.AddValue(name, value);
+                }
+            }
+
+            return propertyContainer;
+        }
+    }
+}
---
+++ WebSite/Budiseng.DapperExtensions/IEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Budiseng.DapperExtensions
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+         
+    }
+}

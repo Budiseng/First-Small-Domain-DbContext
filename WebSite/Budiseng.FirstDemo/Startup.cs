d0585c0e052350fc4e85200b577245dbb2e9800f
budaiseng <budaiseng@email.com>
2015-04-11 13:23:37 +0800

    init
    Signed-off-by: budaiseng <budaiseng@email.com>

---
+++ WebSite/Budiseng.FirstDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Budiseng.FirstDemo.Startup))]
+namespace Budiseng.FirstDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
